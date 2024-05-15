cce49733767f2f9c3d5734f98992084bdeb382fe
Kamilla <62097619+KamillaT@users.noreply.github.com>
2024-05-15 15:23:04 +0300

    Updated personal information

--- opd.ru/www/after_login/update_table_data.php
+++ opd.ru/www/after_login/update_table_data.php
@@ -2,12 +2,15 @@
 // Handle POST request
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Get the selected value and text from the POST data
-    $selectedValue = $_POST['value'];
+    // $selectedValue = $_POST['value'];
     $username = $_POST['id'];
+    $admin = $_POST['admin'];
+    $expert = $_POST['expert'];
     // $selectedText = $_POST['text'];
 
     $conn = new mysqli("localhost", "root", "", "users");
-    $conn->query("UPDATE user_data SET state = '$selectedValue' WHERE username = '$username'");
+    $conn->query("UPDATE user_data SET admin = $admin WHERE username = '$username'");
+    $conn->query("UPDATE user_data SET expert = $expert WHERE username = '$username'");
     // Update the database with the selected value
     // Replace this with your actual database update logic
     // For demonstration purposes, we will just return the selected value and text
